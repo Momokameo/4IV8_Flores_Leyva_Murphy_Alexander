1e1a0b5911596cb39d34ffa7c07d9ff1f2c5cb1a
Momokameo <72054645+Momokameo@users.noreply.github.com>
2021-03-27 17:57:35 -0600

    .

---
+++ js/main.js
@@ -0,0 +1,10 @@
+﻿ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.cards',{ delay: 600});
+ScrollReveal().reveal('.footer',{ delay: 600});
+ScrollReveal().reveal('.gods-cards',{ delay: 600});
+ScrollReveal().reveal('.recomend',{ delay: 600});
+ScrollReveal().reveal('.footer-footer',{ delay: 500});
+
+document.querySelector('.menu-btn').addEventListener('click', ()=> {
+    document.querySelector('.nav-menu').classList.toggle('show');
+})
